ac23b885b49d793c2ad9b9fb74b6b6fcdef7aae5
zahrashirazi <zahra.r.shirazi1375@gmail.com>
2020-12-10 17:41:57 +0330

    add view, templates, urls

--- Authentication/views.py
+++ Authentication/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def login_page_view(request, *args, **kwargs):
+    context = {}
+    return render(request=request, template_name='LoginPage.html', context=context, content_type=None,
+                  status=None,
+                  using=None)
+
+
+def signup_page_view(request, *args, **kwargs):
+    context = {}
+    return render(request=request, template_name='SignUpPage.html', context=context, content_type=None,
+                  status=None,
+                  using=None)
+
+
+def payment_page_view(request, *args, **kwargs):
+    context = {}
+    return render(request=request, template_name='PaymentPage.html', context=context, content_type=None,
+                  status=None,
+                  using=None)
+
+
+def user_panel_main_page_view(request, *args, **kwargs):
+    context = {}
+    return render(request=request, template_name='UserPanelMainPage.html', context=context, content_type=None,
+                  status=None,
+                  using=None)
--- Store/views.py
+++ Store/views.py
@@ -7,3 +7,31 @@ def user_panel_main_page_view(request, *args, **kwargs):
     return render(request=request, template_name='HomePage.html', context=context, content_type=None,
                   status=None,
                   using=None)
+
+
+def all_books_page_view(request, *args, **kwargs):
+    context = {}
+    return render(request=request, template_name='AllBooksPage.html', context=context, content_type=None,
+                  status=None,
+                  using=None)
+
+
+def detail_books_page_view(request, *args, **kwargs):
+    context = {}
+    return render(request=request, template_name='DetailBookPage.html', context=context, content_type=None,
+                  status=None,
+                  using=None)
+
+
+def contact_us_page_view(request, *args, **kwargs):
+    context = {}
+    return render(request=request, template_name='ContactUs.html', context=context, content_type=None,
+                  status=None,
+                  using=None)
+
+
+def payment_page_view(request, *args, **kwargs):
+    context = {}
+    return render(request=request, template_name='PaymentPage.html', context=context, content_type=None,
+                  status=None,
+                  using=None)
