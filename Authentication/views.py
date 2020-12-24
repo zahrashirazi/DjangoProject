e435baecdfd6c674b3477d181d5b1f7a9e22e036
ali akhtari <zahra.r.shirazi1375@gmail.com>
2020-12-24 21:29:49 +0330

    fix bugs, add pages

--- Authentication/views.py
+++ Authentication/views.py
@@ -118,17 +118,6 @@ def payment_page_view(request, *args, **kwargs):
     context = {}
 
     if request.user.is_authenticated:
-        books = []
-        try:
-            number = Cart.objects.get(user=request.user).number
-            items = Cart.objects.get(user=request.user).Item.all()
-            for item in items:
-                books.append(item)
-        except Cart.DoesNotExist:
-            number = 0
-        context['Number'] = number
-        context['Books'] = books
-
         if request.method == 'POST':
             try:
                 cart = Cart.objects.get(user=request.user)
@@ -138,6 +127,17 @@ def payment_page_view(request, *args, **kwargs):
                 context['ALERT'] = 'Your payment paid successfully.'
             except:
                 pass
+
+            books = []
+            try:
+                number = Cart.objects.get(user=request.user).number
+                items = Cart.objects.get(user=request.user).Item.all()
+                for item in items:
+                    books.append(item)
+            except Cart.DoesNotExist:
+                number = 0
+            context['Number'] = number
+            context['Books'] = books
 
     return render(request=request, template_name='PaymentPage.html', context=context, content_type=None,
                   status=None,
